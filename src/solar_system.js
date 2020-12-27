1f20e068861ed08fc37bf0d88f23e6a8bebc2ee5
Bernardo <programming@bernardomg.com>
2020-12-27 21:13:03 +0100

    Removed margin

--- src/solar_system.js
+++ src/solar_system.js
@@ -1,13 +1,10 @@
-var margin = { top: 100, right: 50, bottom: 100, left: 50 };
-
-var width = 960 - margin.left - margin.right,
-  height = 500 - margin.top - margin.bottom;
+var width = 960,
+  height = 500;
 
 var svg = d3.select("body").append("svg")
-  .attr("width", width + margin.left + margin.right)
-  .attr("height", height + margin.top + margin.bottom)
-  .append("g")
-  .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+  .attr("width", width)
+  .attr("height", height)
+  .append("g");
 
 var solar = [
   {
